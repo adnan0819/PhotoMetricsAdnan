d70b6f97a0241e601c47eee8c136b9c2415f632c
Adnan Firoze <adnanfiroze@Adnans-MacBook-Pro.local>
2016-04-03 08:02:47 -0400

    Added scoring engine w/ JSON output and batch processing

--- batch.m
+++ batch.m
@@ -1,14 +1,17 @@
 function [saliency, gridPoints, centroidOfSalientROI, dist, score, normed, normed2,pics] = batch()
     
     pics = dir('TestImages/*.jpg');
+     fprintf('{');
 for k = 1:numel(pics)
    img = imread(pics(k).name);
    [saliency, gridPoints, centroidOfSalientROI, dist, score, normed] = main(img);
    normed2(k)=normed;
    
    %disp(normed);
-   fprintf('{"id": %d, "name": "%s" , "score:" %d, \n', k, pics(k).name, ceil(normed));
-   fprintf('"');
+  
+   fprintf('"id": %d, "name": "%s" , "score:" %d, \n', k, pics(k).name, ceil(normed));
+
    %json='"id:';
    %disp(json);
 end
+   fprintf('}');
